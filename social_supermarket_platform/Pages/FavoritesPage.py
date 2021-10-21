b268dc1ed64e3c152a6de7e4b6697b72db5845b1
vtkachenko <vtkachenko@llnw.com>
2021-10-21 12:57:50 +0300

    add tests for Favorite Page

--- social_supermarket_platform/Pages/FavoritesPage.py
+++ social_supermarket_platform/Pages/FavoritesPage.py
@@ -2,17 +2,21 @@ from selenium.webdriver.common.by import By
 from Config.config import TestData
 from Pages.BasePage import BasePage
 
-class SendAgift(BasePage):
+class FavouritesPage(BasePage):
     """ locators - """
-    PAGE_TITLE = '//div[@class="ProductCatalogue__Title-sc-1kai38p-3 kmefkK"]'
-    SEARCH_FOR_A_PRODUCT = '//input[@type="text"]'
-    FAVOURITES = '//button[@class="CategoryButton__Button-atybu6-0 kKZNSc"]'
-    OTHER_CATEGORIES = '//button[@class="CategoryButton__Button-atybu6-0 igAitb"]'
-    PRICE_FILTER = '//div[@class="PriceFilter__Container-sc-1ip33kp-0 coSEZl"]'
-    LOAD_MORE_BUTTON = '//button[@class="PrimaryButton__Button-sc-1aj0s98-1 bPcAIM"]'
-    GO_UP_BUTTON = '//button[@class="RoundIconButton__BackTopButton-uci0o1-0 hwnGi"]'
-    PRODUCTS_LIST = '//div[@class="ProductList__Products-jbi448-2 kzUUHv"]'
-    PRODUCT_FROM_LIST = '//div[@class="ProductCard__Container-pavi5k-0 dCgtmR"]'
-    PLUS_BUTTON = '//i[@class="plus icon"][1]'
-    SEARCH_ICON = '//i[@class="search icon"][1]'
-    FAVORITE_ICON = '//i[@class="favorite icon"][1]'
+    FAVORITES_PAGE = '//a[@href="/favorites"]'
+    PRODUCT_LIST = '//div[@class="ProductList__Container-jbi448-0 iewNfG"]'
+    SEARCH_ICON = '//div[@class="FavoritesPage__Container-sc-1tq5yuv-0 cjtyME"]//i[@class="search icon"][1]'
+    FAVORITE_ICON = '//div[@class="FavoritesPage__Container-sc-1tq5yuv-0 cjtyME"]//i[@class="favorite icon"][1]'
+    MODAL_CLOSE_BUTTON = '//div[@class="Modal__Close-sc-10kpwz6-4 brEnYU"]'
+    MODAL_TITLE = '//h1[@class="ProductModal__Title-sc-18obu0p-4 jMAsjx"]'
+    MODAL_DETAILS_BUTTON = '//button[@class="PlainButton-hzadhp-0 AccordionHeader__HeaderButton-sc-33meak-1 boLGoi cOIUGB"]//span[text()="Details"]'
+    MODAL_ABOUT_BUTTON = '//button[@class="PlainButton-hzadhp-0 AccordionHeader__HeaderButton-sc-33meak-1 boLGoi cOIUGB"]//span[text()="About "]'
+
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver.get(TestData.BASE_URL)
+        self.do_login(TestData.USERNAME, TestData.PASSWORD)
+        self.click(self.FAVORITES_PAGE)
+        self.wait_till_loader_disappear()
---
+++ social_supermarket_platform/Tests/smoke_tests/test_Favorites.py
@@ -0,0 +1,28 @@
+from Tests.base import BaseTest
+from Pages.FavoritesPage import FavouritesPage
+
+class TestFavorites(BaseTest):
+    def test_verify_all_elements_at_favorites_page(self):
+        """
+        This test case verify:
+         - each elements visible, clickable at Favorites page.
+         - each element im product modal is visible, clickable
+        """
+        self.favorites = FavouritesPage(self.driver)
+
+        assert self.favorites.is_visible(self.favorites.PRODUCT_LIST)
+        assert self.favorites.is_visible(self.favorites.SEARCH_ICON)
+        assert self.favorites.is_clickable(self.favorites.SEARCH_ICON)
+        assert self.favorites.is_clickable(self.favorites.FAVORITE_ICON)
+        assert self.favorites.is_visible(self.favorites.FAVORITE_ICON)
+
+        # click SEARCH_ICON to open modal
+        self.favorites.click(self.favorites.SEARCH_ICON)
+
+        assert self.favorites.is_visible(self.favorites.MODAL_TITLE)
+        assert self.favorites.is_visible(self.favorites.MODAL_DETAILS_BUTTON)
+        assert self.favorites.is_clickable(self.favorites.MODAL_DETAILS_BUTTON)
+        assert self.favorites.is_visible(self.favorites.MODAL_ABOUT_BUTTON)
+        assert self.favorites.is_clickable(self.favorites.MODAL_ABOUT_BUTTON)
+        assert self.favorites.is_clickable(self.favorites.MODAL_CLOSE_BUTTON)
+        assert self.favorites.is_visible(self.favorites.MODAL_CLOSE_BUTTON)
